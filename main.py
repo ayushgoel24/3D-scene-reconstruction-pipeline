cb4e065a9b8822ecebae57f3f1224cce05381f89
ayushgoel24 <ayush.goel2427@gmail.com>
2023-01-02 12:41:57 +0530

    optical flow functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from environment import ApplicationProperties
 from src.sfm2view import ReconstructionFrom2DImages
+from src.flow import ComputeFlow
 
 applicationProperties = ApplicationProperties("application.yml")
 applicationProperties.initializeProperties()
@@ -7,3 +8,6 @@ applicationProperties.initializeProperties()
 ## 3D Reconstruction from 2D Images ##
 reconstructionFrom2DImages = ReconstructionFrom2DImages( applicationProperties )
 reconstructionFrom2DImages.complete_pipeline()
+
+opticalFlow = ComputeFlow( applicationProperties )
+opticalFlow.complete_pipeline()
