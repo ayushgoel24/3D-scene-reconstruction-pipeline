094675337878b0fbbc3d30468a8861c397d9a558
ayushgoel24 <ayush.goel2427@gmail.com>
2023-01-03 22:20:29 +0530

    main.py updated

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import argparse
 from environment import ApplicationProperties
 from src.sfm2view import ReconstructionFrom2DImages
 from src.flow import ComputeFlow
+from src.stereo import Stereo
 
-applicationProperties = ApplicationProperties("application.yml")
-applicationProperties.initializeProperties()
+if __name__ == "__main__":
 
-## 3D Reconstruction from 2D Images ##
-reconstructionFrom2DImages = ReconstructionFrom2DImages( applicationProperties )
-reconstructionFrom2DImages.complete_pipeline()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pipeline", action="store_true")
 
-opticalFlow = ComputeFlow( applicationProperties )
-opticalFlow.complete_pipeline()
+    args = parser.parse_args()
+
+    applicationProperties = ApplicationProperties("application.yml")
+    applicationProperties.initializeProperties()
+
+    if args.pipeline == "SfM":
+        ## 3D Reconstruction from 2D Images ##
+        reconstructionFrom2DImages = ReconstructionFrom2DImages( applicationProperties )
+        reconstructionFrom2DImages.complete_pipeline()
+
+    elif args.pipeline == "flow":
+        opticalFlow = ComputeFlow( applicationProperties )
+        opticalFlow.complete_pipeline()
+
+    else:
+        stereo = Stereo( applicationProperties )
+        stereo.complete_pipeline()
